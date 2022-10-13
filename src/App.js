4012db85d4f7f9c201f88b1055a3ef1a778ac0ad
My Name <mdarfinji45@gmail.com>
2022-10-13 20:56:42 +0800

    adding conditional rendering

--- src/App.js
+++ src/App.js
@@ -2,17 +2,46 @@
 import './App.css';
 
 
-import { getAuth, GoogleAuthProvider} from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut} from "firebase/auth";
 import app from './firebase.init';
+import { useState } from 'react';
 
 const auth = getAuth(app);
 
 function App() {
 
+const [user , setUser] = useState({});
 
   const handleGoogleSignIn = () => {
 
-    console.log("object");
+ signInWithPopup(auth, provider)
+
+ .then(result => {
+
+  const user = result.user;
+ setUser(user)
+
+ })
+ .catch(error => {
+  console.log("object");
+ })
+  }
+
+  const handleGoogleSignOut = ( )  => {
+
+     signOut(auth)
+
+     .then( ( ) => {
+          
+      setUser({});
+
+     })
+     .catch(() => {
+
+      setUser({});
+      
+     })
+
   }
 
   const provider = new GoogleAuthProvider()
@@ -22,6 +51,10 @@ function App() {
   return (
     <div className="App">
        <button onClick={handleGoogleSignIn}>GOOGLE SIGN IN </button>
+       <button onClick={handleGoogleSignOut}>GOOGLE SIGN out </button>
+
+        <h4>user name : {user.displayName}</h4>
+        <p>email : {user.email}</p>
 
     </div>
   );
